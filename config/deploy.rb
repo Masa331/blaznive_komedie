1217fb25b22bf86740338f59425ed5cf1ecde3e2
Premysl Donat <pdonat@seznam.cz>
2016-04-09 11:25:48 +0200

    Update some configs for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,8 @@
 set :application, 'blaznive_komedie'
-# set :repo_url,    'git@bitbucket.org:masa331/blaznive_komedie.git'
 set :repo_url,    'git@github.com:Masa331/blaznive_komedie.git'
 set :deploy_to,   '/var/www/blaznive_komedie'
 set :pty, true
-set :linked_files, %w{config/database.yml config/smtp_settings.yml}
+set :linked_files, %w{config/database.yml config/smtp_settings.yml config/secrets.yml}
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :rails_env, 'production'
@@ -15,15 +14,6 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  task :prepare_sitemaps do
-    on roles(:app) do
-      execute :mkdir, '-p', "#{shared_path}/sitemaps"
-      execute :rm, '-rf', "#{release_path}/public/sitemaps"
-      execute :ln, '-s', "#{shared_path}/sitemaps #{release_path}/public/sitemaps"
-      execute :rake, 'sitemap:generate'
     end
   end
 
