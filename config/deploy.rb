c0561e3fff34e5c3bde8e4019375b0805e5e791e
Premysl Donat <pdonat@seznam.cz>
2016-04-09 11:44:39 +0200

    Update capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, 'blaznive_komedie'
 set :repo_url,    'git@github.com:Masa331/blaznive_komedie.git'
 set :deploy_to,   '/var/www/blaznive_komedie'
 set :pty, true
-set :linked_files, %w{config/database.yml config/smtp_settings.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/smtp_settings.yml config/secrets.yml db/production.sqlite3}
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :rails_env, 'production'
