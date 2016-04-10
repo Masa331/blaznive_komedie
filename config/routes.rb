a7ce33a3c40ea8f6822efc401901843afa0d4869
Premysl Donat <pdonat@seznam.cz>
2016-04-10 12:09:22 +0200

    Disable maintenance page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '*x/*x', to: 'static_pages#under_construction'
-  root 'static_pages#under_construction'
+  # get '*x/*x', to: 'static_pages#under_construction'
+  # root 'static_pages#under_construction'
 
   resources :comedies do
     collection do
