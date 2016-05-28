6bd25389935e914fc492ab985a43e8ff2decb8f7
Premysl Donat <pdonat@seznam.cz>
2016-05-28 11:34:46 +0200

    Better search results page

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -1,9 +1,17 @@
 class SearchResultsController < ApplicationController
   def index
-    @searched_term = params[:search_term]
+    @searched_term = search_param
 
     @comedies_by_title = Comedy.title_search(@searched_term)
     @comedies_by_text = Comedy.where("text LIKE ?", "%#{@searched_term}%")
+
     @tags = ActsAsTaggableOn::Tag.where('name LIKE ?', "%#{@searched_term}%")
+    @comedies_by_tag = Comedy.tagged_with(@tags.map(&:name), any: true)
+  end
+
+  private
+
+  def search_param
+    params.permit(:search_term)[:search_term]
   end
 end
