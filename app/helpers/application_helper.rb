bf902ccfcc346a83ed0e38fa209cca22b2a80ca3
Premysl Donat <pdonat@seznam.cz>
2016-05-28 11:51:23 +0200

    Click on tag in order to search by it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,20 @@ module ApplicationHelper
     Redcarpet::Markdown.new(renderer, {})
   end
 
+  def video_iframe(video)
+    content_tag(:iframe, nil, { src: video.youtube_link.uri,
+                                height: 281,
+                                width: 500,
+                                allowfullscreen: '',
+                                frameborder: 0 })
+  end
+
+  def tag_with_link(tag)
+    content_tag :span, class: "label label-pill label-default" do
+      link_to tag, search_results_path(search_term: tag)
+    end
+  end
+
   private
 
   def renderer
--- app/helpers/videos_helper.rb
+++
@@ -1,9 +0,0 @@
-module VideosHelper
-  def video_iframe(video)
-    content_tag(:iframe, nil, { src: video.youtube_link.uri,
-                                height: 281,
-                                width: 500,
-                                allowfullscreen: '',
-                                frameborder: 0 })
-  end
-end
